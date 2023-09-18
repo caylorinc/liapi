bc319c3999f4bf85f8e30e2bf9a65537ff5497b7
Jason Caylor <jason@caylor.me>
2023-09-17 21:46:48 -0400

    adding some intial unit tests

---
+++ main_test.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+	"time"
+)
+
+func TestGetLiatrio(t *testing.T) {
+	req, err := http.NewRequest("GET", "/liatrio", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(GetLiatrio)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := LiatrioResponse{"Automate all the things!", strconv.FormatInt(time.Now().Unix(), 10)}
+	var actual LiatrioResponse
+	err = json.NewDecoder(rr.Body).Decode(&actual)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !isEqual(expected, actual) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			actual, expected)
+	}
+}
+
+func TestPostLiatrio(t *testing.T) {
+	message := "Hello, Liatrio!"
+	reqBody, err := json.Marshal(LiatrioRequest{message})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", "/liatrio", bytes.NewBuffer(reqBody))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(PostLiatrio)
+
+	handler.ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusCreated {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusCreated)
+	}
+
+	expected := LiatrioResponse{message, strconv.FormatInt(time.Now().Unix(), 10)}
+	var actual LiatrioResponse
+	err = json.NewDecoder(rr.Body).Decode(&actual)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !isEqual(expected, actual) {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			actual, expected)
+	}
+}
+
+func isEqual(expected, actual LiatrioResponse) bool {
+	return expected.Message == actual.Message && expected.Timestamp == actual.Timestamp
+}
